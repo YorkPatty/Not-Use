fa6fd01e97dc2c1c3ee3cfbf7f30259c0cd28e0c
jonathanwozny6 <71037154+jonathanwozny6@users.noreply.github.com>
2021-03-16 16:57:18 -0400

    Update GenerateMPCDesignsTry1.m

--- GenerateMPCDesignsTry1.m
+++ GenerateMPCDesignsTry1.m
@@ -1,119 +1,117 @@
-% note that EGR and VGT are the throttle and wastegate in our case (input)s
-% MAP is our output values from the data
-
-% EGR (in) --> IDdata{i}.simin.signals.values(:,1)
-% VGT --> IDdata{i}.simin.signals.values(:,2)
-% Speed --> IDdata{i}.simin.signals.values(:,4)
-% MAP --> IDdata{i}.simout.signals.values(:,7)
-% EGR (out) --> IDdata{i}.simout.signals.values(:,14)
-
-% Things to do/figure out:    
-    % understand weights and min/max rate of change ('RateMin' and 'RateMax')    
-    % try to include a second unmeasured disturbance
-    % understand how the validation works
-    % automate implementation of the multiple mpc's into the system
-        % idk if we're expected to automate this though
-    
-%Create system identification models
 function [MPCobjs,Fits,Validations]=GenerateMPCDesignsTry1(iddataCollection,vddataCollection)
+% This function takes simulated input/output data and creates state space
+% models. 
 
-   for i=1:length(iddataCollection(1,:)),
+% Inputs: The i/o data will be throttle and wastegate position and engine
+%         speed as the inputs. MAP (intake manifold pressure) will be the 
+%         output. 
+    % IDData: i/o data used for system identification (model creation)
+    % VDData: i/o data that will be used for model validation
+
+% Outputs: 
+    % MPCobjs: MPC objects created from the identified model
+    % Fits: time, measured outputs, simulated outputs (w/ ID'd model)
+    % Validations: same as above but with validation data
+         
+% Output Data: 
+    % MAP (intake manifold pressure)
+
+    for i=1:length(iddataCollection(1,:)),
        
-      %Plant fit 
-    %  FuelMass=1000.*IDdata{i}.simout.signals.values(:,11)./(IDdata{i}.simin.signals.values(:,3).*(2.*IDdata{i}.simin.signals.values(:,4)/60.));  %Calculate fuel mass from AFR and airflow data
-     
-      %Set up system identification input data for ThrottlePos and Speed
-     % JUST UNPACKAGING IDDATA (inputs)
-      u=[iddataCollection{i}.signals.values(:,1:2)]; % iddataCollection{i}.signals.values(:,2)]; 
-        %  FuelMass 
-        %  IDdata{i}.simin.signals.values(:,4)];  
+      %Plant fit    
+      % unpackage identification data (take out Thr and Wg and assign to
+      % new variable)
+      u = [iddataCollection{i}.signals.values(:,1)]; % add Wg (:,1:2)
       
-     % Set up system identification output data for MAP
-     % JUST UNPACKAGING IDDATA (outputs)
+     % unpackaging output (MAP)
       y=iddataCollection{i}.signals.values(:,3); 
       
-      % set to >2 based on just looking at DataAnalyzer and seeing where it
-      % stops growing
-      % JUST CLEANING UP THE DATA TO GET PURE STEADY STATE RESPONSE
-      ind=find(iddataCollection{i}.time(:,1)>0.5);  %Find initial steady-state start point to remove initial engine startup transient
-      z=iddata(y(ind,:),u(ind,:));
-      z.Ts=0.01; % same sampling time as current system
+      % clean up data and create iddata object
+      % set value of 2 chosen based on examination of data inspector
+      ind=find(iddataCollection{i}.time(:,1)>2);  %Find initial steady-state start point to remove initial engine startup transient
+      z=iddata(y(ind,:),u(ind,:)); % create iddata object with i/o data
+      z.Ts=0.01; % same sampling time as SI Engine Dyno system
       
-      % creating fourth order state space models using system identifaction
-      % toolbox
-      IDmodel=n4sid(z,4,'Ts',0.01,'DisturbanceModel','none');
+      % create specified order state space models
+        % order of 5 works well for throttle to MAP
+      IDmodel=n4sid(z,5,'Ts',0.01,'DisturbanceModel','none');
       
-      % simulate the response (yout) of the identified model(state space); returns output
-      % response of the state space using input values 
+      % simulate the response (yout) of the identified model(state space); 
       yout=sim(IDmodel,z.InputData); % yout is the outputs from the model
       time=(0:size(yout,1)-1)'*z.Ts;
       
-      % just saving the fits for later comparison (outputs of the function)
-      Fits{i}=[time z.OutputData(:,1) yout(:,1)]; %z.OutputData(:,2) yout(:,2)];
+      % Saving the time, measured and simulated outputs for later analysis
+      % FUNCTION OUTPUT
+      Fits{i}=[time z.OutputData(:,1) yout(:,1)]; % add more depending on number of outputs
       
       %Design MPC Control
-      MPCobjs{i}=DesignMPC(IDmodel,z);     
-      %MPCobjs{i}=mpc(IDmodel,z);  
+      MPCobjs{i}=DesignMPC(IDmodel,z); % FUNCTION OUTPUT
       
-      %Plant validation
-   %   FuelMass=1000.*VData{i}.simout.signals.values(:,11)./(VData{i}.simin.signals.values(:,3).*(2.*VData{i}.simin.signals.values(:,4)/60.));  %Calculate fuel mass from AFR and airflow data
-     
-   u=[vddataCollection{i}.signals.values(:,1:2)];% vddataCollection{i}.signals.values(:,2)];
-        % FuelMass 
-        % VData{i}.simin.signals.values(:,4)];  %Set up system identification input data for EGR, VGT, FUEL, SPEED
+      %Plant validation   
+      % unpackage data
+      u=[vddataCollection{i}.signals.values(:,1)];% add 2 for (:,1:2)
       y=vddataCollection{i}.signals.values(:,3); %Set up system identifaction output data for MAP, EGR
       ind=find(vddataCollection{i}.time>0.5);  %Find initial steady-state start point to remove initial engine startup transient
-      z=iddata(y(ind,:),u(ind,:));
-      z.Ts=0.01;
-      yout=sim(IDmodel,z.InputData);
+      z=iddata(y(ind,:),u(ind,:)); % create iddata object
+      z.Ts=0.01; % set sample time
+      yout=sim(IDmodel,z.InputData); % simulate output response of model
       time=(0:size(yout,1)-1)'*z.Ts;
-      Validations{i}=[time z.OutputData(:,1) yout(:,1)]; % z.OutputData(:,2) yout(:,2)];
+      % save output data for later analysis (output of function)
+      Validations{i}=[time z.OutputData(:,1) yout(:,1)]; % FUNCTION OUTPUT
       
+      % validate with 
+          % y is the simulated output response
+          % NRMSE best fit value (same best fit as SID Toolbox 'model output')
+          % ic: initial conditions
+          [y fits ic] = compare(z, IDmodel);
+          BestFit(i) = fits; % store to see fit of each MPC
    end
  
 end
 
 
 function MPCobj=DesignMPC(IDmodel,IDdata)
+% This function takes the state spaces models and creates the
+% corresponding number of controllers. Each MPC is reviewed and then 
+% open for further tuning in the MPCDesigner app. 
 
+% Inputs: 
+    % IDmodel: created state space model
+    % IDdata: workspace data from GetIDdata for system ID
+    
+% Outputs:
+    % MPCobj: mpc object ready for design
+    
+   % create the plant
    Plant = ss(IDmodel);
    Plant.InputName='';
    Plant.InputUnit='';
    
    % Augment plant with unmeasured disturbance channel.
-   set(Plant, 'B', [Plant.B Plant.B(:,1)], 'D', [Plant.D Plant.D(:,1)]);
+   % set(Plant, 'B', [Plant.B Plant.B(:,1)], 'D', [Plant.D Plant.D(:,1)]);
    
    % Assign names to I/O variables
-   % INPUTS: 
-        % EGRPOS AND VGTPOS  --> replace with THRPOS
-        % FUELMASS and SPEED --> measured disturbances (DELETE FUEL MASS)
-        % UD1 and UD2        --> unmeasured disturbances (KEEP)
-        
-   % OUTPUTS:
-        % BOOST and EGRMASSFLOW --> keep BOOST (MAP) and delete EGRMASSFLOW
-   set(Plant, 'InputName', {'THRPOS'; 'SPEED'; 'UD1'},...
+   set(Plant, 'InputName', {'THRPOS'},... % {'THRPOS'; 'WGPOS'; 'ENGSPD'; 'UD1'; 'UD2'}
     'OutputName', {'MAP'},...
-    'InputUnit', {'%', 'rpm', ''},...
+    'InputUnit', {'%'},... % {'%', '%', 'RPM', '', ''}
     'OutputUnit', {'KPa'});
-    
-    % MAKE INTO setmpcsignals(Plant, 'mv', [1], 'md', [2], 'ud', [3,4],   'mo', [1]);
-    %                                 ThrPos    EngSpd      UD1 and UD2    MAP
-    %                              
+   
+                               
     % Assign MPC channel type
-    % Plant = setmpcsignals(Plant,'mv',[1 2],'md',[3 4],'ud',[5 6],'mo',[1 2]);
-    setmpcsignals(Plant, 'mv', [1],'md', [2], 'ud', [3],'mo', [1]);
+    % Plant = setmpcsignals(Plant,'mv',[1 2],'md',[3],'ud',[4 5],'mo',[1]);
+    setmpcsignals(Plant, 'mv', [1],'mo', [1]);
     Model.Plant = Plant;
     
     %Set nomimal inputs and outputs obtained at the equilibrium operating point
-    Model.Nominal.U=[mean(IDdata.u) 0];
+    Model.Nominal.U=[mean(IDdata.u)]; % [mean(IDdata.u 0 0]
     Model.Nominal.Y=mean(IDdata.y);
     Model.Nominal.X=[0 0 0 0];
     Model.Nominal.DX=[0 0 0 0];
 
     %% Define sample time and horizons
-    Ts=0.2; % sample time
-    p=20;   % prediction time is about 4 seconds
-    m=2;    % 10% of prediction horizon
+    Ts=0.05; % sample time
+    p=40;   % prediction time is about 4 seconds --> 
+    m=4;    % 10% of prediction horizon
 
     %% Define hard and soft constraints
     % EGRPOS, VGTPOS move full-range approx 1 second, FUELMASS can move
@@ -125,9 +123,9 @@ function MPCobj=DesignMPC(IDmodel,IDdata)
     %OutputSpecs(2)=struct('Min',0.8*min(IDdata.y(:,2)),'Max',1.2*max(IDdata.y(:,2)),'MinECR',1,'MaxECR',1);
 
     %% Define weights on manipulated and controlled variables.
-    Weights=struct('ManipulatedVariables',[0,0,0],...
-       'ManipulatedVariablesRate',[1,1,1],...
-       'OutputVariables',[0.1]);
+    Weights=struct('ManipulatedVariables',[0],... % [0 0]
+       'ManipulatedVariablesRate',[1],...         % [1 1]
+       'OutputVariables',[0.1]);                  
 
     %% Create MPC controller in MATLAB
     MPCobj=mpc(Model,Ts,p,m,Weights,InputSpecs,OutputSpecs);
